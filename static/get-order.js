def302223ae5bb0d006178030efba9f822a7beab
Pete West <west@diffblue.com>
2017-02-02 15:32:50 +0000

    Reload page after setting cookie

--- static/get-order.js
+++ static/get-order.js
@@ -2,5 +2,6 @@ var request = new XMLHttpRequest();
 request.open('GET', 'https://deliveroobot.herokuapp.com/order/' + btoa(window.location.pathname), true);
 request.onload = function (data) {
     document.cookie = 'basket='+btoa(request.responseText)+'; path=/';
+    window.location.reload();
 };
 request.send();
