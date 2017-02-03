264cbcd074ae2893435fcc8efe9bc173be966962
Pete West <west@diffblue.com>
2017-02-03 11:57:26 +0000

    Update bookmarklet to be more cross-browser compatible

--- static/submit-order.js
+++ static/submit-order.js
@@ -1,10 +1,10 @@
 var request = new XMLHttpRequest();
 request.open('POST', 'https://deliveroobot.herokuapp.com/order', true);
 request.setRequestHeader('Content-type', 'application/json');
-var userId = document.cookie.split(/; /).find(function(item) { return item.match(/^roo_guid=/); }).replace(/^roo_guid=/, '')
+var userId = document.cookie.split(/; /).filter(function(item) { return item.match(/^roo_guid=/); })[0].replace(/^roo_guid=/, '')
 request.setRequestHeader('User', userId);
 request.onload = function () { alert('Order submitted') };
-var basket = document.cookie.split(/; /).find(function(item) { return item.match(/^basket=/); });
+var basket = document.cookie.split(/; /).filter(function(item) { return item.match(/^basket=/); })[0];
 if (basket) {
     request.send(atob(basket.replace(/^basket=/, '').replace(/\.+$/, '')));
 } else {
